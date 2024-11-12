96786a593aae813e84cb007a58606853a02b9e7a
Gusleme <159180396+Gusleme@users.noreply.github.com>
2024-11-12 13:29:21 -0300

    Add files via upload

---
+++ AT13-ManyToOne/prjMercado/src/main/java/com/senaidev/mercado/PrjMercadoApplication.java
@@ -0,0 +1,13 @@
+package com.senaidev.mercado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PrjMercadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PrjMercadoApplication.class, args);
+	}
+
+}
